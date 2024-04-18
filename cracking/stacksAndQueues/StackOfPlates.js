009d70331b72fa07000e936560ad5930627c41cc
David Spires <dspires@ymail.com>
2024-04-18 10:29:20 -0500

    stack updates

---
+++ cracking/stacksAndQueues/StackOfPlates.js
@@ -0,0 +1,51 @@
+class StackOfPlates {
+    constructor(stackCapacity) {
+        this.stacks = [];
+        this.stackCapacity = stackCapacity;
+    }
+
+    push() {
+        if(!this.stacks.length) {
+            this.stacks[0] = ['plate1'];
+        } else if(this.stacks[this.stacks.length - 1].length === this.stackCapacity) {
+            this.stacks.push([`plate${this.stacks.length + 1}`]);
+        } else {
+            this.stacks[this.stacks.length - 1].push(`plate${this.stacks.length}`);
+        }
+    }
+
+    pop() {
+        if(!this.stacks.length) {
+            return null;
+        } else {
+            let plate = this.stacks[this.stacks.length - 1].pop();
+
+            if(!this.stacks[this.stacks.length - 1].length) {
+                this.stacks.pop();
+            }
+
+            return plate;
+        }
+    }
+
+    print() {
+        for(let i = 0; i < this.stacks.length; i++) {
+            for(let j = 0; j < this.stacks[i].length; j++) {
+                console.log(this.stacks[i][j]);
+            }
+        }
+    }
+
+}
+
+const plates = new StackOfPlates(3);
+
+for(let i = 0; i < 20; i++) {
+    plates.push();
+}
+
+for(let i = 0; i < 5; i++) {
+    plates.pop();
+}
+
+plates.print();
--- cracking/stacksAndQueues/stackMin.js
+++ cracking/stacksAndQueues/stackMin.js
@@ -65,5 +65,78 @@ while(!stack.isEmpty()) {
 console.log('The min of the stack is ', stack.min);
 
 
+class StackWithMinStack {
+    constructor() {
+        this.elements = [];
+        // This will keep track of all possible mins
+        this.minStack = [];
+    }
 
-//TODO: Solve this using an additional stack
+    pop() {
+        if(this.length === 0) {
+            return null;
+        }
+        let element = this.elements.pop();
+        let topOfMinStack = this.minStack[this.minStack.length - 1];
+
+        if(topOfMinStack === element) {
+            this.minStack.pop();
+        }
+
+        return element;
+    }
+
+    push(data) {
+        if(!this.minStack.length) {
+            this.minStack.push(data);
+        } else {
+            let topOfMinStack = this.minStack[this.minStack.length - 1];
+
+            if(data < topOfMinStack) {
+                this.minStack.push(data);
+            }
+        }
+        this.elements.push(data);
+    }
+
+    peek() {
+        if(this.isEmpty()) {
+            return null;
+        }
+
+        return this.elements[this.elements.length - 1];
+    }
+
+    isEmpty() {
+        return this.elements.length === 0;
+    }
+
+    get length() {
+        return this.elements.length;
+    }
+
+    get currentMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
+
+console.log('new stack....');
+
+let stack2 = new StackWithMinStack();
+
+stack2.push(1);
+stack2.push(10);
+stack2.push(-100);
+stack2.push(-1);
+stack2.push(-1300);
+stack2.push(10000);
+stack2.push(12);
+
+console.log('The min of this stack is ', stack2.currentMin);
+
+while(!stack2.isEmpty()) {
+    let element = stack2.pop();
+    console.log(`The min of the stack is after removing ${element} is: `, stack2.currentMin);
+}
+
+console.log('The min of this stack is ', stack2.currentMin);
