c5e275f9dfac17b3751ae47c50d4f670f2ba2ca8
David Spires <dspires@ymail.com>
2024-05-06 11:32:53 -0500

    MaxHeap solved

--- cracking/trees/MaxHeap.js
+++ cracking/trees/MaxHeap.js
@@ -14,19 +14,65 @@ class MaxHeap {
 
     // Rearrange the elements in the heap to maintain the heap property 
     heapify(index) {
-    
+        let left = index * 2 + 1;
+        let right = index * 2 + 2;
+        let largest = index;
+
+        if(this.heap[left] > this.heap[largest] && this.size > left) {
+            largest = left;
+        }
+
+        if(this.heap[right] > this.heap[largest] && this.size > right) {
+            largest = right;
+        }
+
+        if(largest !== index) {
+            let tmp = this.heap[largest];
+            this.heap[largest] = this.heap[index];
+            this.heap[index] = tmp;
+            this.heapify(largest);
+        }
     }
 
     percolateUp(index) {
-        
+        let parent = Math.floor(index - 1 / 2);
+
+        if(index <= 0) {
+            return;
+        } else if(this.heap[index] > this.heap[parent]) {
+            let tmp = this.heap[parent];
+            this.heap[parent] = this.heap[index];
+            this.heap[index] = tmp;
+            this.percolateUp(parent);
+        }
     }
 
     insert(data) {
-      
+      if(!this.size) {
+        this.heap[0] = data;
+        this.size++;
+      } else {
+        this.heap.push(data);
+        this.size++;
+        this.percolateUp(this.size - 1);
+      }
     }
 
     removeMax() {
-   
+        if(this.size === 0) {
+            return;
+        }
+
+        if(this.size === 1) {
+            this.size--;
+            return this.heap[0];
+        }
+
+        let max = this.heap[0];
+        this.heap[0] = this.heap[this.size - 1];
+        this.size--;
+        this.heapify(0);
+        return max;
     }
 
     isEmpty() {
@@ -52,3 +98,23 @@ class MaxHeap {
 
 
 const maxHeap = new MaxHeap();
+maxHeap.insert(0);
+maxHeap.insert(1);
+maxHeap.insert(2);
+maxHeap.insert(-30);
+maxHeap.insert(3);
+maxHeap.insert(100);
+maxHeap.insert(5);
+maxHeap.insert(20);
+
+console.log(maxHeap.heap)
+
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
+console.log(maxHeap.removeMax());
